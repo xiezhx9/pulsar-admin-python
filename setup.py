9368879f35b792228d136a6b288664699f114811
xiezhengxiong <xiezhx9@mail3.sysu.edu.cn>
2025-01-18 11:08:59 -0500

    ci: update Github Actions to use new repository path
    Signed-off-by: xiezhengxiong <xiezhx9@mail3.sysu.edu.cn>

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="pulsar_admin",
     version="0.0.2",
-    author="shoothzj",
-    author_email="shoothzj@gmail.com",
+    author="hezhangjian",
+    author_email="hezhangjian97@gmail.com",
     description="A Python package for interacting with the Pulsar admin API",
     long_description_content_type="text/markdown",
     url="https://github.com/protocol-laboratory/pulsar_admin",
